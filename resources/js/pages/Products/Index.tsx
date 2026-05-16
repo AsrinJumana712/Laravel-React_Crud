7eeb5d64d292d68b7d840de71278ae6321a7ecdf
AsrinJumana712 <s22010084@ousl.lk>
2026-05-16 14:13:32 +0530

    add the flash message alert function

--- resources/js/pages/Products/Index.tsx
+++ resources/js/pages/Products/Index.tsx
@@ -1,7 +1,9 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, usePage } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Megaphone } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,12 +12,31 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface PageProps {
+    flash: {
+        message?: string;
+    };
+}
+
 export default function Index() {
+
+    const { flash } = usePage().props as PageProps;
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
             <div className='m-4'>
                 <Link href={route('products.create')}><Button>Create a product</Button></Link>
+            </div>
+            <div className='m-4'>
+                {flash.message && (
+                    <Alert>
+                        <Megaphone className="h-4 w-4" />
+                        <AlertTitle>Notification!</AlertTitle>
+                        <AlertDescription>
+                            {flash.message}
+                            </AlertDescription>
+                    </Alert>
+                )}
             </div>
         </AppLayout>
     );
