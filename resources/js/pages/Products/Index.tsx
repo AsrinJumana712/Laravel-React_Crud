a526b2cdadde19e58426af1c3a998bd8babdac5c
AsrinJumana712 <s22010084@ousl.lk>
2026-05-16 20:32:55 +0530

    adding delete button with function

--- resources/js/pages/Products/Index.tsx
+++ resources/js/pages/Products/Index.tsx
@@ -1,9 +1,10 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/react';
+import { Head, Link, useForm, usePage } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Megaphone } from 'lucide-react';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,15 +13,32 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
 interface PageProps {
     flash: {
         message?: string;
-    };
+    },
+    products: Product[];
 }
 
 export default function Index() {
 
-    const { flash } = usePage().props as PageProps;
+    const { products, flash } = usePage().props as PageProps;
+
+    const { submit, get, post, put, patch, delete: destroy, processing } = useForm();
+
+    const handleDelete = (id: number, name: string) => {
+        if (confirm(`Are you sure you want to delete the product - ${id}. ${name}`)) {
+            destroy(route('products.destroy', id));
+        }
+    };
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
@@ -34,10 +52,39 @@ export default function Index() {
                         <AlertTitle>Notification!</AlertTitle>
                         <AlertDescription>
                             {flash.message}
-                            </AlertDescription>
+                        </AlertDescription>
                     </Alert>
                 )}
             </div>
+            {products.length > 0 && (
+                <div className="w-8/12 p-4">
+                    <Table>
+                        <TableCaption>A list of your recent products.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">ID</TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Price</TableHead>
+                                <TableHead>Description</TableHead>
+                                <TableHead className="text-center">Actions</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {products.map((product) => (
+                            <TableRow>
+                                <TableCell className="font-medium">{product.id}</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                                <TableCell>{product.price.toFixed(2)}</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                                <TableCell className="text-center">
+                                    <Button disabled={processing} onClick={() => handleDelete(product.id, product.name)} className='bg-red-500 hover:bg-red-700'>Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
         </AppLayout>
     );
 }
