e935624f23b574014dafade379a84e24599413e5
AsrinJumana712 <s22010084@ousl.lk>
2026-05-17 12:09:21 +0530

    updating web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/products/create', [\App\Http\Controllers\ProductController::class, 'create'])->name('products.create');
     Route::post('/products', [\App\Http\Controllers\ProductController::class, 'store'])->name('products.store');
     Route::delete('/products/{product}', [\App\Http\Controllers\ProductController::class, 'destroy'])->name('products.destroy');
-
+    Route::get('/products/{product}', [\App\Http\Controllers\ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [\App\Http\Controllers\ProductController::class, 'update'])->name('products.update');
 });
 
 require __DIR__.'/settings.php';
